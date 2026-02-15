38792ba9c8234494f73d56f1f476acb66fc0eb12
Aleksei <martyniukaleksei0@gmail.com>
2026-02-15 21:27:56 +0200

    Setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// В реальном проекте вы будете импортировать страницы из папки pages
+// import HomePage from "./pages/Home";
+
+function App() {
+  return (
+    <Router>
+      <div style={{ padding: "20px" }}>
+        {/* Навигация (меню) */}
+        <nav>
+          <Link to="/" style={{ marginRight: "10px" }}>
+            Главная
+          </Link>
+          <Link to="/about">О нас</Link>
+        </nav>
+
+        <hr />
+
+        {/* Здесь меняются страницы */}
+        <Routes>
+          <Route path="/" element={<h1>Главная страница (Dashboard)</h1>} />
+          <Route
+            path="/about"
+            element={<h1>О проекте (Сделано 2 разработчиками)</h1>}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
